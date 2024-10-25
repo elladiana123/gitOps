2516fa26b1b5cdd42bc1de96b42a09f14b88bc19
elladiana123 <diondiana55@gmail.com>
2024-10-24 23:07:36 -0600

    update destroy

--- infra-terraform/backend.tf
+++ infra-terraform/backend.tf
@@ -11,3 +11,4 @@ terraform {
 # terraform init -backend-config="backend.tf"
 # terraform plan -var-file="terraform.tfvars"
 # terraform apply -var-file="terraform.tfvars"  
+ # terraform destroy -var-file="terraform.tfvars"
